7e18829393836689f44032bb2eeb560caee539da
Dennis Guiboche <dguiboche2@academic.rrc.ca>
2025-09-24 16:24:03 -0500

    added health check point and health test

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,20 @@ import morgan from 'morgan';
 const app: Express = express();
 const port = 3000;
 
+app.get("/api/v1/health", (_req, res) => {
+    res.json({
+        status: "OK",
+        uptime: process.uptime(),
+        timestamp: new Date().toISOString(),
+        version: "1.0.0",
+    });
+});
+
 app.use(morgan("combined"));
 app.use('/api/v1/routes/', basicRoute);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+export default app;
